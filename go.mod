9bc4c17bb7216db5193f08b6d9792deca39b93f3
koji <y_koji_0807@yahoo.co.jp>
2023-03-28 12:56:16 +0900

    push event handler

--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module forkeys
 go 1.17
 
 require (
+	cloud.google.com/go/pubsub v1.30.0
 	cloud.google.com/go/secretmanager v1.10.0
-	google.golang.org/genproto v0.0.0-20230323212658-478b75c54725
 )
 
 require (
+	cloud.google.com/go v0.110.0 // indirect
 	cloud.google.com/go/compute v1.19.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	cloud.google.com/go/iam v0.13.0 // indirect
@@ -19,10 +20,12 @@ require (
 	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/oauth2 v0.6.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	google.golang.org/api v0.114.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230323212658-478b75c54725 // indirect
 	google.golang.org/grpc v1.54.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,3 +1,103 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	secretmanagerpb "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+)
+
+var (
+	projectName     string
+	eventHandlerURL string
+)
+
+func main() {
+	projectName = os.Getenv("PROJECT_NAME")
+	eventHandlerURL = os.Getenv("WEBHOOK")
+
+	// url := "https://example.com/custom_event"
+	data := map[string]interface{}{
+		"EventType": "event-type",
+		"Data": map[string]string{
+			"key1": "value1",
+			"key2": "value2",
+		},
+	}
+
+	body, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("POST", eventHandlerURL, bytes.NewBuffer(body))
+	if err != nil {
+		panic(err)
+	}
+
+	// Generate signature
+	expectedSignature := "sha1="
+	secret, err := getSecret(projectName, "custom-ev-handler", "latest")
+	if err != nil {
+		panic(err)
+	}
+	hashed := hmac.New(sha1.New, secret)
+	hashed.Write(body)
+	expectedSignature += hex.EncodeToString(hashed.Sum(nil))
+
+	// Set headers
+	req.Header.Set("User-Agent", "sample")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Sample-Event", "sample")
+	req.Header.Set("Sample-Signature", expectedSignature)
+
+	// Send request
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	// Handle response
+	respBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(respBody))
+}
+
+func getSecret(projectName, secretName, versionNum string) ([]byte, error) {
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: fmt.Sprintf("projects/%s/secrets/%s/versions/%s", projectName, secretName, versionNum),
+	}
+
+	result, err := client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.Payload.Data, nil
+}
+
+// これはHTTPリクエストを加えるとエラーになってしまったコード
+
 // package main
 
 // import (
@@ -5,47 +105,67 @@
 // 	"context"
 // 	"crypto/hmac"
 // 	"crypto/sha1"
-// 	"encoding/hex"
-// 	"log"
-// 	"encoding/json"
 // 	"encoding/base64"
+// 	"encoding/hex"
+// 	"encoding/json"
 // 	"fmt"
 // 	"io/ioutil"
+// 	"log"
 // 	"net/http"
 // 	"os"
 
 // 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
-// 	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
+// 	secretmanagerpb "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 // )
 
-// // const secretKey = "your_secret_key"
+// type Payload struct {
+// 	CreatedAt  string `json:"created_at"`
+// 	Repository struct {
+// 		Name string `json:"name"`
+// 		Url  string `json:"url"`
+// 	} `json:"repository"`
+// }
+
+// type SampleEvent struct {
+// 	EventType   string  `json:"event_type"`
+// 	ID          string  `json:"id"`
+// 	Metadata    string  `json:"metadata"`
+// 	TimeCreated string  `json:"time_created"`
+// 	Signature   string  `json:"signature"`
+// 	MsgID       string  `json:"msg_id"`
+// 	Source      string  `json:"source"`
+// 	Payload     Payload `json:"payload"`
+// }
 
 // var (
-// 	// secretKey 			 string
-// 	projectID      	 	 string
-// 	eventHandlerURL      string
+// 	projectName     string
+// 	eventHandlerURL string
 // )
 
 // func main() {
-//  	projectID = os.Getenv("PROJECT_ID")
-// 	// secretKey = os.Getenv("SECRET")
+// 	projectName = os.Getenv("PROJECT_NAME")
 // 	eventHandlerURL = os.Getenv("WEBHOOK")
 
-// 	payload := map[string]interface{}{"key1": "value1", "key2": "value2"}
-// 	payloadBytes, err := json.Marshal(payload)
-// 	if err != nil {
-// 		log.Fatalf("Failed to marshal payload: %v", err)
+// 	payload := Payload{
+// 		CreatedAt: "2022-03-27T09:00:00Z",
+// 		Repository: struct {
+// 			Name string `json:"name"`
+// 			Url  string `json:"url"`
+// 		}{
+// 			Name: "example-repo",
+// 			Url:  "https://github.com/example/repo",
+// 		},
 // 	}
 
 // 	// イベントデータを作成
-// 	data := map[string]interface{}{
-// 		"event_type":  "sample",
-// 		"id":          "your-id",
-// 		"metadata":    "your-metadata",
-// 		"time_created": "your-time-created",
-// 		"msg_id":       "your-msg-id",
-// 		"source":      "sample",
-// 		"payload": 		map[string]interface{}{"key1": "value1", "key2": "value2"},
+// 	data := SampleEvent{
+// 		EventType:   "sample",
+// 		ID:          "your-id",
+// 		Metadata:    "your-metadata",
+// 		TimeCreated: "your-time-created",
+// 		MsgID:       "your-msg-id",
+// 		Source:      "sample",
+// 		Payload:     payload,
 // 	}
 
 // 	// イベントデータを JSON に変換
@@ -54,8 +174,9 @@
 // 		log.Fatalf("Failed to marshal message data: %v", err)
 // 	}
 
+// 	// ハッシュ値を生成
 // 	expectedSignature := "sha1="
-// 	secret, err := getSecret(projectID, "custom-ev-handler", "latest")
+// 	secret, err := getSecret(projectName, "custom-ev-handler", "latest")
 // 	if err != nil {
 // 		panic(err)
 // 	}
@@ -64,39 +185,14 @@
 // 	hashed.Write(msg)
 // 	expectedSignature += hex.EncodeToString(hashed.Sum(nil))
 
-// 	fmt.Println(expectedSignature)
-
 // 	// Pub/Sub形式のメッセージを作成
 // 	pubsubMessage := map[string]interface{}{
 // 		"attributes": map[string]interface{}{
-// 			// "headers": string(json.RawMessage(`{
-// 			// 	"Sample-Event": "` + data["event_type"].(string) + `",
-// 			// 	"Sample-Signature": "` + expectedSignature + `",
-// 			// 	"Content-Type": "application/json"
-// 			// }`)),
-// 			// "headers": string(json.RawMessage(`{
-// 			"Sample-Event": data["event_type"].(string),
+// 			"Sample-Event":     data.EventType,
 // 			"Sample-Signature": expectedSignature,
-// 			"Content-Type": "application/json",
-// 			// }`)),
+// 			"Content-Type":     "application/json",
 // 		},
-// 		"data":       base64.StdEncoding.EncodeToString(payloadBytes),
-// 		// "message": map[string]interface{}{
-// 		// 	"attributes": map[string]interface{}{
-// 		// 		// "headers": string(json.RawMessage(`{
-// 		// 		// 	"Sample-Event": "` + data["event_type"].(string) + `",
-// 		// 		// 	"Sample-Signature": "` + expectedSignature + `",
-// 		// 		// 	"Content-Type": "application/json"
-// 		// 		// }`)),
-// 		// 		// "headers": string(json.RawMessage(`{
-// 		// 		"Sample-Event": data["event_type"].(string),
-// 		// 		"Sample-Signature": expectedSignature,
-// 		// 		"Content-Type": "application/json",
-// 		// 		// }`)),
-// 		// 	},
-// 		// 	"data":       base64.StdEncoding.EncodeToString(payloadBytes),
-// 		// 	"message_id": data["msg_id"].(string),
-// 		// },
+// 		"data": base64.StdEncoding.EncodeToString(msg),
 // 	}
 
 // 	pubsubMessageBytes, err := json.Marshal(pubsubMessage)
@@ -110,29 +206,29 @@
 // 		panic(err)
 // 	}
 // 	req.Header.Set("Content-Type", "application/json")
-// 	req.Header.Set("Sample-Event", data["event_type"].(string))
+// 	req.Header.Set("Sample-Event", data.EventType)
 // 	req.Header.Set("Sample-Signature", expectedSignature)
-// 	req.Header.Set("User-Agent", "sample") // イベントハンドラが受信を許可するソースを設定
-// 	fmt.Println(req)
-// 	// HTTP クライアントを作成してリクエストを送信
-// 	client := &http.Client{}
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer resp.Body.Close()
+// 	req.Header.Set("User-Agent", "sample")
 
-// 	// レスポンスを表示
-// 	fmt.Printf("Status: %s\n", resp.Status)
-
-// 	respBody, err := ioutil.ReadAll(resp.Body)
+// 	// HTTP リクエストを送信
+// 	// client := &http.Client{}
+// 	client := http.DefaultClient
+// 	res, err := client.Do(req)
 // 	if err != nil {
-// 		panic(err)
+// 		log.Fatalf("Failed to send request: %v", err)
 // 	}
-// 	fmt.Println("Response Body:", string(respBody))
+// 	body, err := ioutil.ReadAll(res.Body)
+// 	if err != nil {
+// 		log.Fatalf("Failed to read response body: %v", err)
+// 	}
+// 	defer res.Body.Close()
+// 	log.Printf("Response Body: %v", string(body))
+
+// 	fmt.Println("Response status code:", res.StatusCode)
+
 // }
 
-// func getSecret(projectID, secretName, versionNum string) ([]byte, error) {
+// func getSecret(projectName, secretName, versionNum string) ([]byte, error) {
 // 	ctx := context.Background()
 // 	client, err := secretmanager.NewClient(ctx)
 // 	if err != nil {
@@ -141,7 +237,7 @@
 // 	defer client.Close()
 
 // 	req := &secretmanagerpb.AccessSecretVersionRequest{
-// 		Name: "projects/" + projectID + "/secrets/" + secretName + "/versions/" + versionNum,
+// 		Name: "projects/" + projectName + "/secrets/" + secretName + "/versions/" + versionNum,
 // 	}
 
 // 	result, err := client.AccessSecretVersion(ctx, req)
@@ -151,137 +247,3 @@
 
 // 	return result.Payload.Data, nil
 // }
-
-package main
-
-import (
-	"bytes"
-	"context"
-	"crypto/hmac"
-	"crypto/sha1"
-	"encoding/base64"
-	"encoding/hex"
-	"encoding/json"
-	"log"
-	"net/http"
-	"os"
-
-	secretmanager "cloud.google.com/go/secretmanager/apiv1"
-	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
-)
-
-type Payload struct {
-	CreatedAt  string `json:"created_at"`
-	UpdatedAt  string `json:"updated_at"`
-	Repository struct {
-		Name string `json:"name"`
-		Url  string `json:"url"`
-	} `json:"repository"`
-}
-
-type SampleEvent struct {
-	EventType   string  `json:"event_type"`
-	ID          string  `json:"id"`
-	Metadata    string  `json:"metadata"`
-	TimeCreated string  `json:"time_created"`
-	Signature   string  `json:"signature"`
-	MsgID       string  `json:"msg_id"`
-	Source      string  `json:"source"`
-	Payload     Payload `json:"payload"`
-}
-
-var (
-	projectID       string
-	eventHandlerURL string
-)
-
-func main() {
-	projectID = os.Getenv("PROJECT_ID")
-	eventHandlerURL = os.Getenv("WEBHOOK")
-
-	payload := Payload{
-		CreatedAt: "2022-03-27T09:00:00Z",
-		Repository: struct {
-			Name string `json:"name"`
-			Url  string `json:"url"`
-		}{
-			Name: "example-repo",
-			Url:  "https://github.com/example/repo",
-		},
-	}
-
-	// イベントデータを作成
-	data := SampleEvent{
-		EventType:   "sample",
-		ID:          "your-id",
-		Metadata:    "your-metadata",
-		TimeCreated: "your-time-created",
-		MsgID:       "your-msg-id",
-		Source:      "sample",
-		Payload:     payload,
-	}
-
-	// イベントデータを JSON に変換
-	msg, err := json.Marshal(data)
-	if err != nil {
-		log.Fatalf("Failed to marshal message data: %v", err)
-	}
-
-	// ハッシュ値を生成
-	expectedSignature := "sha1="
-	secret, err := getSecret(projectID, "custom-ev-handler", "latest")
-	if err != nil {
-		panic(err)
-	}
-
-	hashed := hmac.New(sha1.New, secret)
-	hashed.Write(msg)
-	expectedSignature += hex.EncodeToString(hashed.Sum(nil))
-
-	// Pub/Sub形式のメッセージを作成
-	pubsubMessage := map[string]interface{}{
-		"attributes": map[string]interface{}{
-			"Sample-Event":     data.EventType,
-			"Sample-Signature": expectedSignature,
-			"Content-Type":     "application/json",
-		},
-		"data": base64.StdEncoding.EncodeToString(msg),
-	}
-
-	pubsubMessageBytes, err := json.Marshal(pubsubMessage)
-	if err != nil {
-		log.Fatalf("Failed to marshal pubsub message data: %v", err)
-	}
-
-	// HTTP リクエストを作成
-	req, err := http.NewRequest("POST", eventHandlerURL, bytes.NewBuffer(pubsubMessageBytes))
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Sample-Event", data.EventType)
-	req.Header.Set("Sample-Signature", expectedSignature)
-	req.Header.Set("User-Agent", "sample")
-
-
-}
-
-func getSecret(projectID, secretName, versionNum string) ([]byte, error) {
-	ctx := context.Background()
-	client, err := secretmanager.NewClient(ctx)
-	if err != nil {
-		return nil, err
-	}
-	defer client.Close()
-
-	req := &secretmanagerpb.AccessSecretVersionRequest{
-		Name: "projects/" + projectID + "/secrets/" + secretName + "/versions/" + versionNum,
-	}
-
-	result, err := client.AccessSecretVersion(ctx, req)
-	if err != nil {
-		return nil, err
-	}
-
-	return result.Payload.Data, nil
-}
