17d44799ca764cec9da5b3aff4dea7e7b760497d
koji <y_koji_0807@yahoo.co.jp>
2023-03-28 14:06:41 +0900

    bigqueryへの保存

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 
@@ -21,57 +22,75 @@ var (
 	eventHandlerURL string
 )
 
+// type CustomEvent struct {
+// 	EventType   string  `json:"event_type"`
+// 	ID          string  `json:"id"`
+// 	Metadata    string  `json:"metadata"`
+// 	TimeCreated string  `json:"time_created"`
+// 	Custom-Signature   string  `json:"signature"`
+// 	MsgID       string  `json:"msg_id"`
+// 	Source      string  `json:"source"`
+// 	Payload     Payload `json:"payload"`
+// }
+
 func main() {
 	projectName = os.Getenv("PROJECT_NAME")
 	eventHandlerURL = os.Getenv("WEBHOOK")
 
 	// url := "https://example.com/custom_event"
 	data := map[string]interface{}{
-		"EventType": "event-type",
+		"EventType": "custom",
 		"Data": map[string]string{
 			"key1": "value1",
 			"key2": "value2",
 		},
 	}
+	// data := map[string]interface{}{
+	// 	"EventType": "custom",
+	// 	"Data": map[string]string{
+	// 		"key1": "value1",
+	// 		"key2": "value2",
+	// 	},
+	// }
 
 	body, err := json.Marshal(data)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to marshal message data: %v", err)
 	}
 
 	req, err := http.NewRequest("POST", eventHandlerURL, bytes.NewBuffer(body))
 	if err != nil {
-		panic(err)
+		log.Printf("Failed to create request: %v", err)
 	}
 
 	// Generate signature
 	expectedSignature := "sha1="
 	secret, err := getSecret(projectName, "custom-ev-handler", "latest")
 	if err != nil {
-		panic(err)
+		log.Printf("Failed to get secret: %v", err)
 	}
 	hashed := hmac.New(sha1.New, secret)
 	hashed.Write(body)
 	expectedSignature += hex.EncodeToString(hashed.Sum(nil))
 
 	// Set headers
-	req.Header.Set("User-Agent", "sample")
+	req.Header.Set("User-Agent", "custom")
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Sample-Event", "sample")
-	req.Header.Set("Sample-Signature", expectedSignature)
+	req.Header.Set("Custom-Event", "custom")
+	req.Header.Set("Custom-Signature", expectedSignature)
 
 	// Send request
 	client := &http.Client{}
 	res, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		log.Printf("Failed to send request: %v", err)
 	}
 	defer res.Body.Close()
 
 	// Handle response
 	respBody, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		panic(err)
+		log.Printf("Failed to read response body: %v", err)
 	}
 	fmt.Println(string(respBody))
 }
@@ -80,7 +99,7 @@ func getSecret(projectName, secretName, versionNum string) ([]byte, error) {
 	ctx := context.Background()
 	client, err := secretmanager.NewClient(ctx)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create secret manager client: %v", err)
 	}
 	defer client.Close()
 
@@ -90,7 +109,7 @@ func getSecret(projectName, secretName, versionNum string) ([]byte, error) {
 
 	result, err := client.AccessSecretVersion(ctx, req)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to access secret version: %v", err)
 	}
 
 	return result.Payload.Data, nil
