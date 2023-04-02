65484bd858f10a81ec97858c09a9f07ac3e20a75
koji <y_koji_0807@yahoo.co.jp>
2023-04-02 14:10:46 +0900

    2023-04-02

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import (
 	"context"
 	"crypto/hmac"
 	"crypto/sha1"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	secretmanagerpb "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
@@ -22,77 +22,172 @@ var (
 	eventHandlerURL string
 )
 
-// type CustomEvent struct {
-// 	EventType   string  `json:"event_type"`
-// 	ID          string  `json:"id"`
-// 	Metadata    string  `json:"metadata"`
-// 	TimeCreated string  `json:"time_created"`
-// 	Custom-Signature   string  `json:"signature"`
-// 	MsgID       string  `json:"msg_id"`
-// 	Source      string  `json:"source"`
-// 	Payload     Payload `json:"payload"`
-// }
+type CustomEvent struct {
+	EventType   string                 `json:"event_type"`
+	ID          string                 `json:"id"`
+	Metadata    map[string]interface{} `json:"metadata"`
+	TimeCreated time.Time              `json:"time_created"`
+	Signature   string                 `json:"signature"`
+	MsgID       string                 `json:"msg_id"`
+	Source      string                 `json:"source"`
+}
 
 func main() {
-	projectName = os.Getenv("PROJECT_NAME")
-	eventHandlerURL = os.Getenv("WEBHOOK")
+	projectName := os.Getenv("PROJECT_NAME")
+	eventHandlerURL := os.Getenv("WEBHOOK")
+	secret, err := getSecret(projectName, "custom-ev-handler", "latest")
+	if err != nil {
+		log.Fatalf("Failed to get secret: %v", err)
+	}
 
-	// url := "https://example.com/custom_event"
-	data := map[string]interface{}{
-		"EventType": "custom",
-		"Data": map[string]string{
-			"key1": "value1",
-			"key2": "value2",
+	data := CustomEvent{
+		EventType:   "custom",
+		ID:          "abc123",
+		// Metadata:    `{"id":"abc123", "time_created":"` + time.Now().UTC().Format(time.RFC3339Nano) + `","key1":"value1", "key2":"value2"}`,
+		Metadata: map[string]interface{}{
+			"id":"abc123",
+			"time_created": time.Now().UTC().Format(time.RFC3339Nano),
+			"key1":"value1",
+			"key2":"value2",
 		},
+		TimeCreated: time.Now().UTC(),
+		MsgID:       "123",
+		Source:      "custom",
 	}
 	// data := map[string]interface{}{
-	// 	"EventType": "custom",
-	// 	"Data": map[string]string{
-	// 		"key1": "value1",
-	// 		"key2": "value2",
-	// 	},
+	// 	"EventType":   "custom",
+	// 	"ID":          "abc123",
+	// 	"Metadata":    map[string]interface{}{"id": "abc123", "time_created": time.Now().UTC().Format(time.RFC3339Nano), "key1": "value1", "key2": "value2"},
+	// 	"TimeCreated": time.Now().UTC(),
+	// 	"MsgID":       "123",
+	// 	"Source":      "custom",
 	// }
 
-	body, err := json.Marshal(data)
+	jsonData, err := json.Marshal(data)
 	if err != nil {
 		log.Fatalf("Failed to marshal message data: %v", err)
 	}
 
-	req, err := http.NewRequest("POST", eventHandlerURL, bytes.NewBuffer(body))
-	if err != nil {
-		log.Printf("Failed to create request: %v", err)
-	}
+	signature := GenerateSignature(secret, jsonData)
+	fmt.Printf("Signature: %s\n", signature)
 
-	// Generate signature
-	expectedSignature := "sha1="
-	secret, err := getSecret(projectName, "custom-ev-handler", "latest")
+	req, err := http.NewRequest("POST", eventHandlerURL, bytes.NewBuffer(jsonData))
 	if err != nil {
-		log.Printf("Failed to get secret: %v", err)
+		log.Fatalf("Failed to create request: %v", err)
 	}
-	hashed := hmac.New(sha1.New, secret)
-	hashed.Write(body)
-	expectedSignature += hex.EncodeToString(hashed.Sum(nil))
-
-	// Set headers
 	req.Header.Set("User-Agent", "custom")
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Custom-Event", "custom")
-	req.Header.Set("Custom-Signature", expectedSignature)
+	req.Header.Set("Custom-Signature", signature)
 
-	// Send request
+	fmt.Printf("req: %s", req)
+
 	client := &http.Client{}
 	res, err := client.Do(req)
 	if err != nil {
-		log.Printf("Failed to send request: %v", err)
+		log.Fatalf("Failed to send request: %v", err)
 	}
 	defer res.Body.Close()
 
-	// Handle response
 	respBody, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Printf("Failed to read response body: %v", err)
+		log.Fatalf("Failed to read response body: %v", err)
 	}
-	fmt.Println(string(respBody))
+	fmt.Printf("respBody: %s", string(respBody))
+}
+
+// func main() {
+// 	projectName = os.Getenv("PROJECT_NAME")
+// 	eventHandlerURL = os.Getenv("WEBHOOK")
+
+// 	data := CustomEvent{
+// 		EventType:   "custom",
+// 		ID:          "abc123",
+// 		Metadata:    `{"id":"abc123", "time_created":"` + time.Now().UTC().Format(time.RFC3339Nano) + `","key1":"value1", "key2":"value2"}`,
+// 		TimeCreated: time.Now().UTC(),
+// 		MsgID:       "123",
+// 		Source:      "custom",
+// 		// Signature:   "",
+// 	}
+
+// 	// Generate signature
+// 	expectedSignature := "sha1="
+// 	secret, err := getSecret(projectName, "custom-ev-handler", "latest")
+// 	if err != nil {
+// 		log.Printf("Failed to get secret: %v", err)
+// 	}
+// 	jsonData, err := json.Marshal(data)
+// 	if err != nil {
+// 		log.Fatalf("Failed to marshal message data: %v", err)
+// 	}
+
+// 	expectedSignature = GenerateSignature(secret, jsonData)
+// 	data.Signature = expectedSignature
+
+// 	body, err := json.Marshal(data)
+// 	if err != nil {
+// 		log.Fatalf("Failed to marshal message data: %v", err)
+// 	}
+
+// 	req, err := http.NewRequest("POST", eventHandlerURL, bytes.NewBuffer(body))
+// 	if err != nil {
+// 		log.Printf("Failed to create request: %v", err)
+// 	}
+
+// 	// hashed := hmac.New(sha1.New, secret)
+// 	// hashed.Write(jsonData)
+
+// 	fmt.Printf("expectedSignature: %s", expectedSignature)
+
+// 	// hashed := hmac.New(sha1.New, secret)
+
+// 	// // // data.Signature = ""
+// 	// // jsonString, _ := json.Marshal(data)
+// 	// // log.Printf("JSON string: %s", jsonString)
+
+// 	// // Decode the JSON string into an empty interface
+// 	// // hashed = hmac.New(sha1.New, secret)
+// 	// hashed.Write(jsonData)
+// 	// expectedSignature += hex.EncodeToString(hashed.Sum(nil))
+
+// 	// body, err := json.Marshal(data)
+// 	// if err != nil {
+// 	// 	log.Fatalf("Failed to marshal message data: %v", err)
+// 	// }
+
+// 	// req, err := http.NewRequest("POST", eventHandlerURL, bytes.NewBuffer(body))
+// 	// if err != nil {
+// 	// 	log.Printf("Failed to create request: %v", err)
+// 	// }
+// 	req.Header.Set("User-Agent", "custom")
+// 	req.Header.Set("Content-Type", "application/json")
+// 	req.Header.Set("Custom-Event", "custom")
+// 	req.Header.Set("Custom-Signature", data.Signature)
+
+// 	fmt.Printf("req: %s", req)
+
+// 	// Set headers
+
+// 	// Send request
+// 	client := &http.Client{}
+// 	res, err := client.Do(req)
+// 	if err != nil {
+// 		log.Printf("Failed to send request: %v", err)
+// 	}
+// 	defer res.Body.Close()
+
+// 	// Handle response
+// 	respBody, err := ioutil.ReadAll(res.Body)
+// 	if err != nil {
+// 		log.Printf("Failed to read response body: %v", err)
+// 	}
+// 	fmt.Println(string(respBody))
+// }
+
+func GenerateSignature(secret, data []byte) string {
+	mac := hmac.New(sha1.New, secret)
+	mac.Write(data)
+	return fmt.Sprintf("sha1=%x", mac.Sum(nil))
 }
 
 func getSecret(projectName, secretName, versionNum string) ([]byte, error) {
@@ -115,153 +210,162 @@ func getSecret(projectName, secretName, versionNum string) ([]byte, error) {
 	return result.Payload.Data, nil
 }
 
-// これはHTTPリクエストを加えるとエラーになってしまったコード
-
-// package main
-
-// import (
-// 	"bytes"
-// 	"context"
-// 	"crypto/hmac"
-// 	"crypto/sha1"
-// 	"encoding/base64"
-// 	"encoding/hex"
-// 	"encoding/json"
-// 	"fmt"
-// 	"io/ioutil"
-// 	"log"
-// 	"net/http"
-// 	"os"
-
-// 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
-// 	secretmanagerpb "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
-// )
-
-// type Payload struct {
-// 	CreatedAt  string `json:"created_at"`
-// 	Repository struct {
-// 		Name string `json:"name"`
-// 		Url  string `json:"url"`
-// 	} `json:"repository"`
-// }
-
-// type SampleEvent struct {
-// 	EventType   string  `json:"event_type"`
-// 	ID          string  `json:"id"`
-// 	Metadata    string  `json:"metadata"`
-// 	TimeCreated string  `json:"time_created"`
-// 	Signature   string  `json:"signature"`
-// 	MsgID       string  `json:"msg_id"`
-// 	Source      string  `json:"source"`
-// 	Payload     Payload `json:"payload"`
-// }
-
 // var (
 // 	projectName     string
 // 	eventHandlerURL string
 // )
 
+// type Repository struct {
+// 	ID       int    `json:"id"`
+// 	Name     string `json:"name"`
+// 	FullName string `json:"full_name"`
+// }
+
+// type Pusher struct {
+// 	Name  string `json:"name"`
+// 	Email string `json:"email"`
+// }
+
+// type CustomEvent struct {
+// 	Ref        string     `json:"ref"`
+// 	Repository Repository `json:"repository"`
+// 	Pusher     Pusher     `json:"pusher"`
+// 	Signature  string     `json:"-"`
+// }
+
 // func main() {
 // 	projectName = os.Getenv("PROJECT_NAME")
 // 	eventHandlerURL = os.Getenv("WEBHOOK")
 
-// 	payload := Payload{
-// 		CreatedAt: "2022-03-27T09:00:00Z",
-// 		Repository: struct {
-// 			Name string `json:"name"`
-// 			Url  string `json:"url"`
-// 		}{
-// 			Name: "example-repo",
-// 			Url:  "https://github.com/example/repo",
+// 	data := CustomEvent{
+// 		Ref: "refs/heads/main",
+// 		Repository: Repository{
+// 			ID:       1,
+// 			Name:     "repo",
+// 			FullName: "user/repo",
+// 		},
+// 		Pusher: Pusher{
+// 			Name:  "user",
+// 			Email: "user@example.com",
 // 		},
 // 	}
 
-// 	// イベントデータを作成
-// 	data := SampleEvent{
-// 		EventType:   "sample",
-// 		ID:          "your-id",
-// 		Metadata:    "your-metadata",
-// 		TimeCreated: "your-time-created",
-// 		MsgID:       "your-msg-id",
-// 		Source:      "sample",
-// 		Payload:     payload,
-// 	}
+// 	// Generate signature
 
-// 	// イベントデータを JSON に変換
-// 	msg, err := json.Marshal(data)
+// 	secret, err := getSecret(projectName, "custom-ev-handler", "latest")
+// 	if err != nil {
+// 		log.Fatalf("Failed to get secret: %v", err)
+// 	}
+// 	hashed := hmac.New(sha1.New, secret)
+
+// 	data.Signature = ""
+// 	jsonString, _ := json.Marshal(data)
+// 	hashed.Write([]byte(jsonString))
+// 	data.Signature = "sha1=" + hex.EncodeToString(hashed.Sum(nil))
+
+// 	log.Printf("data.Signature: %s", data.Signature)
+
+// 	// Encode JSON as Base64
+// 	// jsonData, err := json.Marshal(data)
+// 	// if err != nil {
+// 	// 	log.Fatalf("Failed to marshal JSON data: %v", err)
+// 	// }
+
+// 	// Encode JSON as Base64
+// 	encodedData := base64.StdEncoding.EncodeToString(jsonString)
+// 	message := map[string]interface{}{
+// 		"data": encodedData,
+// 		"attributes": map[string]string{
+// 			"headers": "{}",
+// 		},
+// 		"message_id":   "123",
+// 		"publish_time": "2022-09-29T12:34:56.789Z",
+// 	}
+// 	body, err := json.Marshal(message)
 // 	if err != nil {
 // 		log.Fatalf("Failed to marshal message data: %v", err)
 // 	}
 
-// 	// ハッシュ値を生成
-// 	expectedSignature := "sha1="
-// 	secret, err := getSecret(projectName, "custom-ev-handler", "latest")
+// 	req, err := http.NewRequest("POST", eventHandlerURL, bytes.NewBuffer(body))
 // 	if err != nil {
-// 		panic(err)
+// 		log.Printf("Failed to create request: %v", err)
 // 	}
 
-// 	hashed := hmac.New(sha1.New, secret)
-// 	hashed.Write(msg)
-// 	expectedSignature += hex.EncodeToString(hashed.Sum(nil))
+// 	// Hash the encoded payload with the secret
+// 	// hash := hmac.New(sha1.New, secret)
+// 	// hash.Write([]byte(encodedData))
+// 	// signature := hex.EncodeToString(hash.Sum(nil))
+// 	// secret, err := getSecret(projectName, "custom-ev-handler", "latest")
+// 	// if err != nil {
+// 	// 	log.Printf("Failed to get secret: %v", err)
+// 	// }
+// 	// hashed := hmac.New(sha1.New, secret)
 
-// 	// Pub/Sub形式のメッセージを作成
-// 	pubsubMessage := map[string]interface{}{
-// 		"attributes": map[string]interface{}{
-// 			"Sample-Event":     data.EventType,
-// 			"Sample-Signature": expectedSignature,
-// 			"Content-Type":     "application/json",
-// 		},
-// 		"data": base64.StdEncoding.EncodeToString(msg),
-// 	}
+// 	// // Set data.Signature to an empty string for signature calculation
+// 	// data.Signature = ""
+// 	// jsonString, _ := json.Marshal(data)
+// 	// hashed.Write(jsonString)
+// 	// data.Signature = "sha1=" + hex.EncodeToString(hashed.Sum(nil))
 
-// 	pubsubMessageBytes, err := json.Marshal(pubsubMessage)
-// 	if err != nil {
-// 		log.Fatalf("Failed to marshal pubsub message data: %v", err)
-// 	}
+// 	// log.Printf("data.Signature: %s", data.Signature)
 
-// 	// HTTP リクエストを作成
-// 	req, err := http.NewRequest("POST", eventHandlerURL, bytes.NewBuffer(pubsubMessageBytes))
-// 	if err != nil {
-// 		panic(err)
-// 	}
+// 	// // Encode JSON as Base64
+// 	// encodedData := base64.StdEncoding.EncodeToString(jsonString)
+// 	// message := map[string]interface{}{
+// 	// 	"message": map[string]interface{}{
+// 	// 		"data":         encodedData,
+// 	// 		"attributes":   map[string]string{"headers": "{}"},
+// 	// 		"message_id":   "123",
+// 	// 		"publish_time": "2022-09-29T12:34:56.789Z",
+// 	// 	},
+// 	// }
+// 	// body, err := json.Marshal(message)
+// 	// if err != nil {
+// 	// 	log.Fatalf("Failed to marshal message data: %v", err)
+// 	// }
+
+// 	// req, err := http.NewRequest("POST", eventHandlerURL, bytes.NewBuffer(jsonData))
+// 	// if err != nil {
+// 	// 	log.Printf("Failed to create request: %v", err)
+// 	// }
+
+// 	// Set headers
+// 	req.Header.Set("User-Agent", "custom")
 // 	req.Header.Set("Content-Type", "application/json")
-// 	req.Header.Set("Sample-Event", data.EventType)
-// 	req.Header.Set("Sample-Signature", expectedSignature)
-// 	req.Header.Set("User-Agent", "sample")
+// 	req.Header.Set("Custom-Event", "custom")
+// 	req.Header.Set("Custom-Signature", data.Signature)
 
-// 	// HTTP リクエストを送信
-// 	// client := &http.Client{}
-// 	client := http.DefaultClient
+// 	// Send request
+// 	client := &http.Client{}
 // 	res, err := client.Do(req)
 // 	if err != nil {
-// 		log.Fatalf("Failed to send request: %v", err)
-// 	}
-// 	body, err := ioutil.ReadAll(res.Body)
-// 	if err != nil {
-// 		log.Fatalf("Failed to read response body: %v", err)
+// 		log.Printf("Failed to send request: %v", err)
 // 	}
 // 	defer res.Body.Close()
-// 	log.Printf("Response Body: %v", string(body))
 
-// 	fmt.Println("Response status code:", res.StatusCode)
-
+// 	// Handle response
+// 	respBody, err := ioutil.ReadAll(res.Body)
+// 	if err != nil {
+// 		log.Printf("Failed to read response body: %v", err)
+// 	}
+// 	fmt.Println(string(respBody))
 // }
 
 // func getSecret(projectName, secretName, versionNum string) ([]byte, error) {
 // 	ctx := context.Background()
 // 	client, err := secretmanager.NewClient(ctx)
 // 	if err != nil {
-// 		return nil, err
+// 		return nil, fmt.Errorf("failed to create secret manager client: %v", err)
 // 	}
 // 	defer client.Close()
 
 // 	req := &secretmanagerpb.AccessSecretVersionRequest{
-// 		Name: "projects/" + projectName + "/secrets/" + secretName + "/versions/" + versionNum,
+// 		Name: fmt.Sprintf("projects/%s/secrets/%s/versions/%s", projectName, secretName, versionNum),
 // 	}
 
 // 	result, err := client.AccessSecretVersion(ctx, req)
 // 	if err != nil {
-// 		return nil, err
+// 		return nil, fmt.Errorf("failed to access secret version: %v", err)
 // 	}
 
 // 	return result.Payload.Data, nil
